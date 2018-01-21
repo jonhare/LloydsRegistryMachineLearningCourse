ef13e652b5b11a4583edec749ae0d9130f820635
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2018-01-21 15:05:02 +0000

    fixes

--- Thursday/practical-part1/keras-mnist-bettercnn-vis-filters1.py
+++ Thursday/practical-part1/keras-mnist-bettercnn-vis-filters1.py
@@ -13,7 +13,7 @@ weights = model.layers[0].get_weights()[0]
 # plot the first layer features
 for i in xrange(0,30):
 	plt.subplot(5,6,i+1)
-	plt.imshow(weights[i][0], cmap=plt.get_cmap('gray'))
+	plt.imshow(weights[i,:,:,0], cmap=plt.get_cmap('gray'))
 
 # show the plot
 plt.show()
